e6215e814ca77e8449a7a4c8ce082236bc55f4cb
Sourav Chhimpa <souravchhimpa1@gmail.com>
2024-10-07 20:21:02 +0530

    few text added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,11 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="w-full h-screen flex items-center justify-center">
-      <h1 className="font-bold sm:text-5xl text-3xl">Sourav</h1>
+      <div>
+          <h1 className="font-bold sm:text-9xl text-5xl text-center uppercase text-transparent bg-clip-text bg-cover bg-center bg-[url('/text-backgound.webp')]">
+          dev Sourav</h1>
+          <h3 className="uppercase text-orange-700 font-semibold text-3xl text-center">soon</h3>
+      </div>
     </div>
   );
 }
