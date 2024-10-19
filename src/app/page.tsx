b1dab5bebf6e5eeba95012bbd8c9b4f4bad279bb
Sourav Chhimpa <souravchhimpa1@gmail.com>
2024-10-19 22:34:24 +0530

    new random push to solve production error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
   return (
     <div className="w-full mx-auto">
       <HeroSection/>
+      <div className="w-full h-screen bg-red-400"></div>
     </div>
   );
 }
