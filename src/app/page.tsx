e262bcebc94b95cd9649f1b24b04e717e00d56bf
Sourav Chhimpa <souravchhimpa1@gmail.com>
2024-10-19 16:23:24 +0530

    hero section linked with the index page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,10 @@
+import HeroSection from "@/Components/HeroSection";
 
 
 export default function Home() {
   return (
-    <div className="w-full h-screen flex items-center justify-center">
-      <div>
-          <h1 className="font-bold sm:text-9xl text-5xl text-center uppercase text-transparent bg-clip-text bg-cover bg-center bg-[url('/text-backgound.webp')]">
-          dev Sourav</h1>
-          <h3 className="uppercase text-orange-700 font-semibold text-3xl text-center">soon</h3>
-      </div>
+    <div className="w-full mx-auto">
+      <HeroSection/>
     </div>
   );
 }
