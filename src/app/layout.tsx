b36dd159746704d93047ad75a5f827f02c0ca5e1
Sourav Chhimpa <souravchhimpa1@gmail.com>
2024-10-20 11:08:21 +0530

    navbar few bugs fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import Head from "next/head";
+import AnimatedFloatingNavbar from "@/Components/Navbar";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -36,6 +37,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <AnimatedFloatingNavbar/>
         {children}
       </body>
     </html>
