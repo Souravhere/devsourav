ea39a3640fc779db5141bffdd9d33a223121de29
Sourav Chhimpa <souravchhimpa1@gmail.com>
2024-10-23 22:59:31 +0530

    code ready to adedd in to production

--- src/Components/RecentProjects.tsx
+++ src/Components/RecentProjects.tsx
@@ -76,7 +76,7 @@ export function RecentProjects() {
   return (
     <section className="bg-white py-16">
       <div className="container mx-auto px-4">
-        <h1 className="text-3xl md:text-4xl font-bold text-center mb-12 text-gray-800">Recent Projects</h1>
+        <h1 className="text-3xl md:text-4xl font-bold text-center sm:mb-12 mb-4 text-gray-800">My Recent Projects</h1>
         <div className="flex w-full items-center justify-center flex-wrap mx-auto">
           {projects.map((project, index) => (
             <ProjectCard key={index} {...project} />
