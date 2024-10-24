3433a34435739604ee1ae5b1bd02fd560dc0a192
Sourav Chhimpa <souravchhimpa1@gmail.com>
2024-10-24 15:34:30 +0530

    some ui fixed reday to use

--- src/Components/Intractivetext.tsx
+++ src/Components/Intractivetext.tsx
@@ -4,7 +4,7 @@ import { Cover } from "./ui/cover";
 export function CoverDemo() {
   return (
     <div className="bg-white -mt-8">
-      <h1 className="text-4xl md:text-4xl lg:text-6xl font-semibold max-w-7xl mx-auto text-center mt-6 relative z-20 py-6 bg-clip-text text-transparent bg-gradient-to-b from-black via-gray-700 to-gray-900">
+      <h1 className="text-4xl md:text-4xl lg:text-6xl font-semibold max-w-7xl mx-auto text-center mt-6 relative z-20 py-6 bg-clip-text text-transparent bg-gradient-to-b from-black via-gray-700 to-gray-600">
         Build amazing websites <br /> with <Cover>sourav</Cover>
       </h1>
     </div>
