8fbec4ab6641dde4c042e943385937cd1650e86b
Sourav Chhimpa <souravchhimpa1@gmail.com>
2024-11-07 21:38:40 +0530

    background and cards content added

---
+++ src/app/ui/backgrounds/pages.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function pages() {
+  return (
+    <div>
+       we will working on this background page we will come soon 
+    </div>
+  )
+}
+
+export default pages
---
+++ src/app/ui/cards/pages.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function pages() {
+  return (
+    <div>
+        we are working on the cards page we will come soon 
+    </div>
+  )
+}
+
+export default pages
