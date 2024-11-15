b847974f971c6ba1ea40ae62d6f8ccd7b959757b
Sourav Chhimpa <souravchhimpa1@gmail.com>
2024-11-15 23:38:21 +0530

    few bugs fixed and ui improved

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { FeaturesSectionDemo } from "@/Components/FunProject";
 import { CoverDemo } from "@/Components/Intractivetext";
 import EnhancedAnimatedHeroWithGrid from "@/Components/NewHeroSec"
+import { OtherProject } from "@/Components/Otherprojects";
 import OurServices from "@/Components/Ourservices";
 
 import { RecentProjects } from "@/Components/RecentProjects";
@@ -17,6 +18,7 @@ export default function Home() {
         <CoverDemo/>
         <FeaturesSectionDemo/>
         <TrustedCompanies/>
+        <OtherProject/>
         <div className="px-7 py-10">
           <h1 className="text-4xl sm:text-5xl lg:text-6xl font-bold py-2 text-center mb-4 bg-clip-text text-transparent bg-gradient-to-b from-gray-900 via-gray-700 to-gray-600">Coming Soon</h1>
           <p className="text-center text-black">I am working hard to bring more components soon. Bookmark this page to stay tuned!</p>
