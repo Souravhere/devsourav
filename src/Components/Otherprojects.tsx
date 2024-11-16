af8a9f8de9505de38afd02e05b2521cbb9033442
Sourav Chhimpa <souravchhimpa1@gmail.com>
2024-11-16 14:32:04 +0530

    other project beta version completed

--- src/Components/Otherprojects.tsx
+++ src/Components/Otherprojects.tsx
@@ -4,7 +4,7 @@ export function OtherProject() {
   const projects = [
     {
       slug: "https://souravhere.github.io/Veggie-Smart/",
-      date: "28th March, 2024",
+      date: "1 months ago",
       title: "Veggie Smart",
       description:
         "Veggie Smart is a comprehensive tool designed to assist shopkeepers and customers in calculating vegetable prices with ease. This web application allows users to input the price per kilogram and the quantity in both kilograms and grams to get the total price.",
@@ -12,7 +12,7 @@ export function OtherProject() {
     },
     {
       slug: "https://ez-modules.vercel.app/",
-      date: "12th April, 2024",
+      date: "3 months ago",
       title: "EZ Modules",
       description:
         "EZ Modules is a versatile and modern React component library developed with the aim of simplifying the web development experience. It offers a diverse range of UI and feature-based components that are both reusable and customizable, making it easier for developers to build high-quality web applications efficiently.",
@@ -20,7 +20,7 @@ export function OtherProject() {
     },
     {
       slug: "https://funwithsourav.vercel.app/",
-      date: "5th May, 2024",
+      date: "2 months ago",
       title: "Fun With Sourav",
       description:
         "Welcome to Fun With Sourav! This project showcases the latest in web technology and animation. Created by Sourav Chhimpa, a passionate web developer from India, this repository aims to provide animated page views complete with source code for anyone interested in learning, exploring, and contributing.",
@@ -28,7 +28,7 @@ export function OtherProject() {
     },
     {
       slug: "https://gemini-ai-gules.vercel.app/",
-      date: "20th June, 2024",
+      date: "3 months ago",
       title: "Gemini Ai",
       description:
         "Welcome to Gemini Ai, a React-based clone of Google's Gemini AI, leveraging the Gemini API to offer users an interactive and dynamic AI experience. This project aims to replicate and enhance the functionalities of the original Gemini AI, providing an accessible platform for users to explore and engage with advanced AI capabilities.",
@@ -43,7 +43,7 @@ export function OtherProject() {
       </h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 px-4">
         {projects.map((project, index) => (
-          <Link key={index} href={project.slug} passHref>
+          <Link key={index} href={project.slug} target="_blank" passHref>
             <div className="relative overflow-hidden h-full rounded-2xl transition duration-200 group bg-white hover:shadow-xl border border-zinc-100 cursor-pointer">
               <div className="w-full aspect-w-16 aspect-h-10 bg-gray-100 rounded-tr-lg rounded-tl-lg overflow-hidden xl:aspect-w-16 xl:aspect-h-10 relative"></div>
               <div className="p-4">
